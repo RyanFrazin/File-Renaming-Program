0533aebfa00731144506b965d3af729befdf0b16
Ryan Frazin <RyanFrazin@Gmail.com>
2025-01-15 20:50:34 -0800

    Program successfully copies from one directory and pastes to a destination. Should the file name already exist in the destination, it renames the file to a random file name.

--- File Renaming Program/Source.cpp
+++ File Renaming Program/Source.cpp
@@ -1,18 +1,36 @@
-#include<iostream>
-#include<fstream>
-#include <string>
+#include<iostream>;
+#include<filesystem>;
+#include<fstream>;
+#include<string>;
+#include<cstdio>;
+namespace fs = std::filesystem;
 using namespace std;
 
 int main() {
-	string sourceFileName, destinationFileName;
-
-	cout << "Enter the name of the source file: ";
-	cin >> sourceFileName;
-	cout << "Enter the name of the destination file: ";
-	cin >> destinationFileName;
-
-	ifstream sourceFile(sourceFileName);
-	ofstream destinationFile(destinationFileName);
+	std::string sourceFolder;
+	fs::path destinationFolder;
+	cout << "Enter the path of the source directory: ";
+	cin >> sourceFolder;
+	cout << "Enter the path of the output director: ";
+	cin >> destinationFolder;
 
 
+	if (fs::exists(sourceFolder) && fs::is_directory(sourceFolder)) {
+		for (const auto& entry : fs::directory_iterator(sourceFolder)) {
+			fs::path destPath = destinationFolder / entry.path().filename();
+			
+			while (fs::exists(destPath)) {
+				std::string newName = "file_" + std::to_string(time(nullptr)) + entry.path().extension().string();
+				destPath = destinationFolder / newName;
+			}
+			
+			fs::copy(entry.path(), destPath);
+			cout << entry.path().string() << " has been copied successfully." << endl;
+		}
+	} else {
+		cerr << "The provided path is not a valid directory." << endl;
+	}
+
+
+	return 0;
 }
